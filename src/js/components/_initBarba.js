78fa7c63510b00d0e4218d17467ac800740d9350
Валерий <kaktuz34@gmail.com>
2018-01-21 17:57:52 +0200

    added barba.js, initBarba.js, gsap

---
+++ src/js/components/_initBarba.js
@@ -0,0 +1,7 @@
+import Barba from 'barba.js';
+import { TweenLite } from 'gsap';
+import { DOC } from './../_constants';
+
+DOC.ready(() => {
+  Barba.Pjax.start();
+});
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,1 +1,2 @@
 import './_contact-map';
+import './_initBarba';
