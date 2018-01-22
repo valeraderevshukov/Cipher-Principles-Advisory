f504e7b1106c80209aec7250a57e076bffcd5ad9
Валерий <kaktuz34@gmail.com>
2018-01-22 13:55:32 +0200

    update initmap with 'transitionCompleted'

--- src/js/components/_initBarba.js
+++ src/js/components/_initBarba.js
@@ -3,6 +3,9 @@ import Barba from 'barba.js';
 import { DOC } from './../_constants';
 
 DOC.ready(() => {
+  const pageContact = 'contact';
   Barba.Pjax.start();
-  Barba.Dispatcher.on('transitionCompleted', () => window.initMap() );
+  Barba.Dispatcher.on('transitionCompleted', (currentStatus) => {
+    if (currentStatus.namespace === pageContact) window.initMap();
+  } ); 
 });
